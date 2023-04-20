a87590e2c9fef7ca575767270bb8ad72fd4a61c2
janak-12345 <115694395+janak-12345@users.noreply.github.com>
2023-04-20 11:58:54 +0530

    Add

--- fraction.js
+++ fraction.js
@@ -1,1 +1,11 @@
-
+class Fraction {
+  constructor(numerator, denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
+  
+  add(other) {
+    const newNumerator = (this.numerator * other.denominator) + (other.numerator * this.denominator);
+    const newDenominator = this.denominator * other.denominator;
+    return new Fraction(newNumerator, newDenominator);
+  }
